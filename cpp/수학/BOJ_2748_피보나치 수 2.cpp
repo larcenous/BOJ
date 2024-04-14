af7db2ab084c03ca6d61afe588ae0f79c1e20b1b
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-04-14 14:03:31 +0900

    edit BOJ_2748

--- cpp/수학/BOJ_2748_피보나치 수 2.cpp
+++ cpp/수학/BOJ_2748_피보나치 수 2.cpp
@@ -1,24 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int fibo_arr[91]; //declaration global array for DP
-
-int fibo(int n){
-    if(fibo_arr[n] != -1){
-        return fibo_arr[n];
-    }
-    fibo_arr[n] = fibo(n-1) + fibo(n-2);
-    return fibo_arr[n];
-}
-
 int main(){
     int n;
-    for(int i=0; i<91; i++){ //initialize
-        fibo_arr[i] = -1;
-    }
+    //dp배열 선언할때 int로 선언하면 범위가 초과 되기 때문에 long long
+    long long fibo_arr[91]; //declaration global array for DP
     fibo_arr[0] = 0;
     fibo_arr[1] = 1;
     cin >> n;
-    fibo(n);
+    for(int j=2; j<=n; j++){
+        fibo_arr[j] = fibo_arr[j-1] + fibo_arr[j-2];
+    }
     cout << fibo_arr[n];
 }
