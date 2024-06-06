f7f1dd484851b684d53dbab201e764faa978cf6f
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-06-06 22:11:31 +0900

    [Bronze II] Title: 문자열 분석, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ Python/백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -3,13 +3,14 @@ while True:
         ans = [0] * 4
         temp = input()
         for i in range(len(temp)):
-            if temp[i].islower():
+            num = ord(temp[i])
+            if num >= 97 :
                 ans[0] += 1
-            elif temp[i].isupper():
+            elif num >= 65 :
                 ans[1] += 1
-            elif temp[i].isnumeric():
+            elif num >= 48 :
                 ans[2] += 1
-            elif temp[i].isspace():
+            elif num == 32:
                 ans[3] += 1
         print(' '.join(map(str, ans)))
     except:
