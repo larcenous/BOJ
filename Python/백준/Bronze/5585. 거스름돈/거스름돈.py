8f041fe6838144e8981fed17808a13bb98345b97
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-06-20 01:17:59 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,21 @@
+N = 1000 - int(input())
+ans = 0
+while N/500 >= 1 :
+    N -= 500
+    ans += 1
+while N/100 >= 1 :
+    N -= 100
+    ans += 1
+while N/50 >= 1 :
+    N -= 50
+    ans += 1
+while N/10 >= 1 :
+    N -= 10
+    ans += 1
+while N/5 >= 1 :
+    N -= 5
+    ans += 1
+while N/1 >= 1 :
+    N -= 1
+    ans += 1
+print(ans)
