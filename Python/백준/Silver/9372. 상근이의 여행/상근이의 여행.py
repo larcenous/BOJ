0acfca1e9141255e9ea53ffe60122b01aeb8dac4
Geonwoo, Shin <matt0621@korea.ac.kr>
2024-07-29 00:24:06 +0900

    [Silver IV] Title: 상근이의 여행, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    for j in range(m):
+        a, b = map(int, input().split())
+    print(n - 1)
